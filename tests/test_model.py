8648bb81932b08f0c53564a2ebb2d2bb318f2ff3
Avni1410 <seloteavni@gmail.com>
2026-09-06 18:27:12 +0530

    Demonstrate CI test failure

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,7 +6,7 @@ from src.train import train_model
 def test_model_training():
     accuracy = train_model()
 
-    assert accuracy >= 0.80
+    assert accuracy >= 1.10
 
 
 def test_model_file_exists():
