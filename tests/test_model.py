2dbcb2fbf5a1aab306851a28f19859b3cf3d21ae
Avni1410 <seloteavni@gmail.com>
2026-09-06 18:28:54 +0530

    Fix model quality test

--- tests/test_model.py
+++ tests/test_model.py
@@ -6,7 +6,7 @@ from src.train import train_model
 def test_model_training():
     accuracy = train_model()
 
-    assert accuracy >= 1.10
+    assert accuracy >= 0.80
 
 
 def test_model_file_exists():
